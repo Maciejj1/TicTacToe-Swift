517de9fbb28e2fbab38bf392292b296c3c3c487a
Maciej Wawryszuk <89411648+Maciejj1@users.noreply.github.com>
2024-05-03 16:25:23 +0200

    Initial Commit

---
+++ TicTacToeSwiftUI/TicTacToeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeSwiftUIApp.swift
+//  TicTacToeSwiftUI
+//
+//  Created by Maciej on 03/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
